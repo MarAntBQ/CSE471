1940e379166552f2eafa926b759c0589a3d6ffa5
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2022-03-22 23:59:58 -0500

    MileStone week 12 almost done the changes!!!!

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,12 +1,28 @@
 document.getElementById("menu-btn").addEventListener("click", event => {
     let menu = document.getElementById("mobile-menu");
+    let main = document.getElementsByTagName("MAIN")[0];
     if (menu.classList.contains("active")) {
         menu.classList.add("desactive");
+        main.classList.remove("active");
         setTimeout(function () {
             menu.classList.remove("desactive");
             menu.classList.remove("active");
         }, 450);
     } else {
         menu.classList.add("active");
+        main.classList.add("active");
     }
 });
+
+function closeMenu() {
+    let menu = document.getElementById("mobile-menu");
+    let main = document.getElementsByTagName("MAIN")[0];
+    if (menu.classList.contains("active")) {
+        menu.classList.add("desactive");
+        main.classList.remove("active");
+        setTimeout(function () {
+            menu.classList.remove("desactive");
+            menu.classList.remove("active");
+        }, 450);
+    }
+}
